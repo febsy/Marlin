6e9405e9fbfaf564ebc3e5e8a335dbba5be88d23
febsy <fabian_schwager@hotmail.com>
2019-12-16 18:55:20 +0100

    Enabled EEPROM Changed Position of Probe

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -917,7 +917,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { -25, -36, -3.68 }
+#define NOZZLE_TO_PROBE_OFFSET { -25, -36, -1.78 }
 
 // Certain types of probes need to stay away from edges
 #define MIN_PROBE_EDGE 10
@@ -1397,7 +1397,7 @@
  *   M501 - Read settings from EEPROM. (i.e., Throw away unsaved changes)
  *   M502 - Revert settings to "factory" defaults. (Follow with M500 to init the EEPROM.)
  */
-//#define EEPROM_SETTINGS     // Persistent storage with M500 and M501
+#define EEPROM_SETTINGS     // Persistent storage with M500 and M501
 //#define DISABLE_M503        // Saves ~2700 bytes of PROGMEM. Disable for release!
 #define EEPROM_CHITCHAT       // Give feedback on EEPROM commands. Disable to save PROGMEM.
 #if ENABLED(EEPROM_SETTINGS)
